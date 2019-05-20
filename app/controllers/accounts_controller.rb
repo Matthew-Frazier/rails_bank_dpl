ae4aca60ee8dd7e3467306b97636927c1c4254bb
Matthew Frazier <matthewfrazier1988@gmail.com>
2019-05-20 13:11:56 -0600

    finished class assignment

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,13 +1,52 @@
 class AccountsController < ApplicationController
+  before_action :set_account, only: [:show, :edit, :update, :destroy]
+
+
   def index
+    @accounts = current_user.accounts
   end
 
   def show
   end
 
   def new
+    @account = Account.new
+  end
+
+  def create
+    @account = current_user.accounts.new(account_params)
+
+    if @account.save
+      flash[:success] = "Account: #{@account.name} Created"
+      redirect_to accounts_path
+    else
+      flash[:error] = "Error: #{@account.errors.full_messages.join("\n")}"
+      render :new
+    end
   end
 
   def edit
   end
+
+  def update
+    if @account.update(account_params)
+      redirect_to account_path(@account)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @account.destroy
+    redirect_to accounts_path
+  end
+
+  private
+    def set_account
+      @account = current_user.accounts.find(params[:id])
+    end
+
+    def account_params
+      params.require(:account).permit(:name, :balance)
+    end
 end
